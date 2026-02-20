8112dce6cd23976e36ed1ee909daf1f6459fc00e
lexamania <torbanowmain@gmail.com>
2026-02-20 13:09:37 +0200

    create mob spawner

--- scripts/Mob.cs
+++ scripts/Mob.cs
@@ -7,18 +7,23 @@ public partial class Mob : CharacterBody3D
     [Export] public float MinSpeed = 10;
     [Export] public float MaxSpeed = 18;
 
+    private Vector3 LocalVelocity;
+
     public void Initialize(Vector3 startPosition, Vector3 playerPosition)
     {
         LookAtFromPosition(startPosition, playerPosition, Vector3.Up);
         RotateY((float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4));
 
         var speed = (float)GD.RandRange(MinSpeed, MaxSpeed);
-        Velocity = Vector3.Forward * speed;
-        Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y);
+        LocalVelocity = Vector3.Forward * speed;
+        LocalVelocity = LocalVelocity.Rotated(Vector3.Up, Rotation.Y);
+
+        Velocity = LocalVelocity;
     }
 
     public override void _PhysicsProcess(double delta)
     {
+        Velocity = LocalVelocity;
         MoveAndSlide();
     }
 
---
+++ scripts/MobSpawner.cs
@@ -0,0 +1,29 @@
+using Godot;
+
+namespace First3dGame;
+
+public partial class MobSpawner : Node3D
+{
+    [Export] public PackedScene MobScene { get; set; }
+
+    private Player _player;
+    private Timer _timer;
+    private PathFollow3D _spawnLocation;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _player = GetParent().GetNode<Player>("Player");
+        _timer = GetNode<Timer>("Timer");
+        _spawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+    }
+
+    private void OnTimerTimeout()
+    {
+        var mob = MobScene.Instantiate<Mob>();
+        _spawnLocation.ProgressRatio = GD.Randf();
+        mob.Initialize(_spawnLocation.Position, _player.Position);
+
+        AddChild(mob);
+    }
+}
